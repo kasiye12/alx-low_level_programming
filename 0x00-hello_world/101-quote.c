3ea648a7ecc0a48a9441c76bcd9625b7802ebcf3
kasiye12 <kasiye620@gmail.com>
2022-12-08 08:50:24 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
  * main - Entry point
  * Return:1
  */
-
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
 	return (1);
 }
