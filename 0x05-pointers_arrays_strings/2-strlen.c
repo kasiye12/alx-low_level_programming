1e0aec71f02b7f8a76c1db4c7d0e9083df0377a1
kasiye12 <kasiye620@gmail.com>
2022-12-19 03:12:38 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen - find legnth of a string
+ * @s: pointer to the string to chech
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	i++;
+
+	return (i);
+}
