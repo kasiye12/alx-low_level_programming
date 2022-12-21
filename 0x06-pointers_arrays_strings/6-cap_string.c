2d2a2893b9aa8b6a97171aa4e2db208ce93e1f06
kasiye12 <kasiye620@gmail.com>
2022-12-21 04:03:45 -0800

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,44 @@
+#include "kasiye.h"
+/**
+ * *cap_string - capitalize words
+ * @str: pointer
+ * @flag: slag
+ * Return: capitalized string
+ */
+
+char *cap_string(char *str)
+{
+	char sep[] = ",\t;\n; .!?\"(){}";
+	int flag, i, kasiye;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		flag = 0;
+
+		if (i == 0)
+		{
+			flag = 1;
+		}
+		else
+		{
+			for (kasiye = 0; sep[kasiye] != '\0'; i++)
+			{
+				if (str[i - 1] == sep[kasiye])
+				{
+					flag = 1;
+					break;
+				}
+			}
+		}
+		if (flag == 1)
+		{
+			if (str[i] <= 'z' && str[i] >= 'a')
+			{
+				str[i] -= ('a' - 'A');
+			}
+		}
+	}
+	return (str);
+}
+
+
