11c1a5f4dfcb7db3003fa6fbaf4f81b38d593db5
kasiye12 <kasiye620@gmail.com>
2023-01-16 03:45:52 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct dog -structure dog
+ * @name: artibute pointer to char name's ddog
+ * @age: artibute floatt age's  dog
+ * @owner: artibute pointer to char owner's dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
