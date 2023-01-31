3914219fe49cf0746c6e54dcda5bba4b6bdca0dd
kasiye12 <kasiye620@gmail.com>
2023-01-30 23:12:55 -0800

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,6 +4,7 @@
 /**
  * free_listint2 - free a list.
  * @head: Address of the first node a list.
+ * Return:i
  */
 
 void free_listint2(listint_t **head)
@@ -16,7 +17,7 @@ void free_listint2(listint_t **head)
 	while (*head != NULL)
 	{
 		temp = (*head)->next;
-		free(*head)->next;
+		free(*head);
 		*head = temp;
 	}
 }
