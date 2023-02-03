ed43c7a5d8fc4208d311c8552212af293f2dc986
kasiye12 <kasiye620@gmail.com>
2023-02-03 03:51:14 -0800

    gsjsf

--- 0x14-bit_manipulation/kasiye.h
+++ 0x14-bit_manipulation/kasiye.h
@@ -1,14 +1,15 @@
 #ifndef KASIYE_H
 #define KASIYE_H
 
-#include <stdio.h>
-#include <stdlib.h>
 int _putchar(char c);
+int _strlen(const char *s);
+int _pow_recursion(int x, inty);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif
